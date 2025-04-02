4104aaf6b11ef3c79081902dea46f5798cba8743
Chauhan-Keval <kevalk292@gmail.com>
2025-04-02 12:37:57 +0530

    Initialize project structure with React, Vite, and basic styling

---
+++ React movie/Project 1/frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  const movies = [
+    { id: 1, title: "The Shawshank Redemption", year: 1994 },
+    { id: 2, title: "The Godfather", year: 1972 },
+    { id: 3, title: "The Godfather: Part II", year: 1974 },
+    { id: 4, title: "The Dark Knight", year: 2008 },
+  ];
+  return <div className="home">
+    <div className="movies-grid">
+      {movies.map()}
+    </div>
+  </div>;
+}
