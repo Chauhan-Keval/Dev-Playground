3d1ba3a913688f85cd9f28b27f65f467bec1e988
Chauhan Keval <kevalk292@gmail.com>
2025-12-09 20:01:06 +0530

    Refactor main.dart to improve app structure and update home widget content

--- flutter_project/frist_app/lib/main.dart
+++ flutter_project/frist_app/lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Kese ho mathur sahab ')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 24, 94, 151),
+        body: Center(child: Text('Hello world !')),
+      ),
+    ),
+  );
 }
